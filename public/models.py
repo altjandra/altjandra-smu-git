c81ad439afa4c12c29fb2bfe973e6983661e538a
iamshaziera99 <90819095+iamshaziera99@users.noreply.github.com>
2021-11-04 17:10:36 +0800

    modified models.py to remove __init__ function

--- public/models.py
+++ public/models.py
@@ -28,18 +28,6 @@ class Class(db.Model):
     total_class_size = db.Column(db.Integer, nullable=False)
     final_quiz_id = db.Column(db.String(100), db.ForeignKey('quiz.quiz_id'), nullable=True)
 
-    def __init__(self, course_id, class_id, trainer_name, class_start_datetime, class_end_datetime, enrolment_start_datetime, enrolment_end_datetime, current_class_size, total_class_size, final_quiz_id):
-        self.course_id = course_id
-        self.class_id = class_id
-        self.trainer_name = trainer_name
-        self.class_start_datetime = class_start_datetime
-        self.class_end_datetime = class_end_datetime
-        self.enrolment_start_datetime = enrolment_start_datetime
-        self.enrolment_end_datetime = enrolment_end_datetime
-        self.current_class_size = current_class_size
-        self.total_class_size = total_class_size
-        self.final_quiz_id = final_quiz_id
-
     def json(self):
         dto = {
             "course_id": self.course_id, 
@@ -71,12 +59,6 @@ class Completed_Courses(db.Model):
     class_id = db.Column(db.String(10), primary_key=True)
     final_quiz_grade = db.Column(db.Float, db.ForeignKey('quiz.quiz_id'), nullable=False)
 
-    def __init__(self, user_name, course_id, class_id, final_quiz_grade):
-        self.user_name = user_name
-        self.course_id = course_id
-        self.class_id = class_id
-        self.final_quiz_grade = final_quiz_grade
-
     def json(self):
         dto = {
             "user_name": self.user_name, 
@@ -96,11 +78,6 @@ class Course(db.Model):
     course_id = db.Column(db.String(10), primary_key=True)
     course_name = db.Column(db.String(100), nullable=False)
     course_desc = db.Column(db.String(1000), nullable=False)
-
-    def __init__(self, course_id, course_name, course_desc):
-        self.course_id = course_id
-        self.course_name = course_name
-        self.course_desc = course_desc
 
     def json(self):
         dto = {
@@ -122,13 +99,6 @@ class Course_Section_Materials(db.Model):
     section = db.Column(db.Integer, primary_key=True) 
     materials = db.Column(db.String(1000), nullable=False)
     quiz_id = db.Column(db.String(100), db.ForeignKey('quiz.quiz_id'),  nullable=False)
-
-    def __init__(self, course_id, class_id, section, materials, quiz_id):
-        self.course_id = course_id
-        self.class_id = class_id
-        self.section = section
-        self.materials = materials
-        self.quiz_id = quiz_id
 
     def json(self):
         dto = {
@@ -154,14 +124,6 @@ class Course_Section_Progress(db.Model):
     section_materials_status = db.Column(db.String(15), nullable=False)
     section_quiz_status = db.Column(db.String(15), nullable=False)
 
-    def __init__(self, user_name, course_id, class_id, section, section_materials_status, section_quiz_status):
-        self.user_name = user_name
-        self.course_id = course_id
-        self.class_id = class_id
-        self.section = section
-        self.section_materials_status = section_materials_status
-        self.section_quiz_status = section_quiz_status
-
     def json(self):
         dto = {
             "user_name": self.user_name, 
@@ -185,12 +147,6 @@ class Employee(db.Model):
     current_designation = db.Column(db.String(64), nullable=False)
     department = db.Column(db.String(64), nullable=False)
 
-    def __init__(self, user_name, employee_name, current_designation, department):
-        self.user_name = user_name
-        self.employee_name = employee_name
-        self.current_designation = current_designation
-        self.department = department
-
     def json(self):
         dto = {
             "user_name": self.user_name, 
@@ -211,13 +167,6 @@ class Enrolment_Request(db.Model):
     course_id = db.Column(db.String(10), db.ForeignKey('class.course_id'), primary_key=True)
     class_id = db.Column(db.String(10), primary_key=True)
     status = db.Column(db.String(100), nullable=False)
-
-    def __init__(self, user_name, course_id, class_id, status):
-        self.user_name = user_name
-        self.course_id = course_id
-        self.class_id = class_id
-        self.status = status
-
 
     def json(self):
         dto = {
@@ -241,14 +190,6 @@ class Overall_Course_Progress(db.Model):
     sections_completed = db.Column(db.Integer, nullable=False)
     current_section = db.Column(db.Integer, nullable=False)
     final_quiz_grade = db.Column(db.Float, nullable=True)
-
-    def __init__(self, user_name, course_id, class_id, sections_completed, current_section, final_quiz_grade):
-        self.user_name = user_name
-        self.course_id = course_id
-        self.class_id = class_id
-        self.sections_completed = sections_completed
-        self.current_section = current_section
-        self.final_quiz_grade = final_quiz_grade
 
     def json(self):
         dto = {
@@ -279,10 +220,6 @@ class Prerequisite(db.Model):
     course_id = db.Column(db.String(10), db.ForeignKey('course.course_id'), primary_key=True)
     prerequisite_id = db.Column(db.String(10), primary_key=True)
 
-    def __init__(self, course_id, prerequisite_id):
-        self.course_id = course_id
-        self.prerequisite_id = prerequisite_id
-    
     def json(self):
         dto = {
             "course_id": self.course_id, 
@@ -299,10 +236,6 @@ class Quiz(db.Model):
 
     quiz_id = db.Column(db.String(100), primary_key=True)
     quiz_pass = db.Column(db.Float, nullable=False)
-
-    def __init__(self, quiz_id, quiz_pass):
-        self.quiz_id = quiz_id
-        self.quiz_pass = quiz_pass
 
     def json(self):
         dto = {
@@ -322,12 +255,6 @@ class Quiz_Questions(db.Model):
     question_no = db.Column(db.Integer, primary_key=True)
     question_type = db.Column(db.String(3), nullable=False)
     question_desc = db.Column(db.String(1000), nullable=False)
-
-    def __init__(self, quiz_id, question_no, question_type, question_desc):
-        self.quiz_id = quiz_id
-        self.question_no = question_no
-        self.question_type = question_type
-        self.question_desc = question_desc
 
     def json(self):
         dto = {
@@ -350,13 +277,6 @@ class Quiz_Question_Options(db.Model):
     option_id = db.Column(db.String(5), primary_key=True)
     option_desc = db.Column(db.String(100), nullable=False)
     option_status = db.Column(db.String(10), nullable=False)
-
-    def __init__(self, quiz_id, question_no, option_id, option_desc, option_status):
-        self.quiz_id = quiz_id
-        self.question_no = question_no
-        self.option_id = option_id
-        self.option_desc = option_desc
-        self.option_status = option_status
 
     def json(self):
         dto = {
