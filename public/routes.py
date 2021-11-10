bb5cde84f8d57525821a7ec8f0a1d26aac424cd1
iamshaziera99 <90819095+iamshaziera99@users.noreply.github.com>
2021-11-11 04:35:38 +0800

    updated routes.py

--- public/routes.py
+++ public/routes.py
@@ -635,3 +635,118 @@ def admin_delete_enrolment_request(user_name, course_id, class_id):
                 "message": "There was an error while deleting the Enrolment Request: " + str(e)
             }
         ), 500
+
+# ----------------------------------------------------------------------------------------------------------------------------- #
+'''Learner: Get all current courses'''
+
+@app.route("/learner_get_all_current_courses/<string:user_name>")
+def learner_get_all_current_courses(user_name):
+    try:
+        current_course_list = Overall_Course_Progress.query.filter_by(user_name=user_name).all()
+
+        # Courses are available
+        if len(current_course_list) != 0:
+            return jsonify(
+                {
+                    "code": 200,
+                    "data": {
+                        "courses": [course.json() for course in current_course_list]
+                    }
+                }
+            ), 200
+
+        # Courses not available
+        return jsonify(
+            {
+                "code": 404,
+                "message": "There are no courses available."
+            }
+        ), 404
+
+    # Error while retrieving courses
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "There was an error while retrieving the courses: " + str(e)
+            }
+        ), 500
+
+# ----------------------------------------------------------------------------------------------------------------------------- #
+'''Admin: View eligible courses '''
+
+@app.route("/view_eligible_courses/<string:user_name>")
+def view_eligible_courses(user_name):
+    try:
+        course_list = Course.query.all()
+        all_learner_current_course = Overall_Course_Progress.query.filter_by(user_name=user_name).all()
+        all_learner_completed_course = Completed_Courses.query.filter_by(user_name=user_name).all()
+
+        qualified_courses = []
+        unqualified_courses = []
+        course_list_only_course_id = []
+
+        # Returns only qualified courses
+        for course in all_learner_current_course:
+            for system_course in course_list:
+                if (course.json()["course_id"] == system_course.json()["course_id"]):
+                    unqualified_courses.append(system_course.json()["course_id"])
+
+        for course in all_learner_completed_course:
+            for system_course in course_list:
+                if (course.json()["course_id"] == system_course.json()["course_id"]):
+                    unqualified_courses.append(system_course.json()["course_id"])
+
+        for system_course in course_list:
+            course_list_only_course_id.append(system_course.json()["course_id"]) 
+
+        qualified_courses = set(course_list_only_course_id) ^ set(unqualified_courses)
+
+        return jsonify({
+            "code": 200,
+            "data": {
+                "qualified_courses": list(qualified_courses),
+                "unqualified_courses": list(unqualified_courses),
+                "course_list": [course.json() for course in course_list],
+
+            }
+        }), 200
+
+
+    # Error while retrieving qualified courses
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "There was an error while retrieving the eligible courses: " + str(e)
+            }
+        ), 500
+
+# ----------------------------------------------------------------------------------------------------------------------------- #
+'''Learner: Submit Enrolment Request'''
+
+@app.route("/submit_enrolment_request", methods=['POST'])
+def submit_enrolment_request():
+    try:
+        data = request.get_json()
+
+        enrolment_request = Enrolment_Request(**data)
+
+        db.session.add(enrolment_request)
+        db.session.commit()
+
+        return jsonify(
+            {
+                "code": 201,
+                "enrolment_request": enrolment_request.json()
+            }
+        ), 201
+
+    # Error while submitting enrolment request
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred while submitting the enrolment request: " + str(e)
+            }
+        ), 500
